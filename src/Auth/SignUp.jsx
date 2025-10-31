7d516eb88b6e0b99c1188412d21f6d8d70510d8f
Raphael877 <ralphpaul877@gmail.com>
2025-10-31 12:49:33 +0100

    updated

--- src/Auth/SignUp.jsx
+++ src/Auth/SignUp.jsx
@@ -279,7 +279,7 @@ export default SignUp;
 
 const SignUp_content = styled.div`
   width: 100%;
-  height: 100vh;
+  height: auto;
   display: flex;
   justify-content: center;
   align-items: center;
