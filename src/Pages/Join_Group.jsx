8f8ffb2d326c2665dbc5e472e9702e8243e1318d
demmy <Omiyejuwon Ademola>
2025-11-06 01:20:52 +0100

    validateeee

--- src/Pages/Join_Group.jsx
+++ src/Pages/Join_Group.jsx
@@ -71,7 +71,12 @@ const Join_Group = () => {
       <div className="circle_down_right"></div>
 
       <div className="brand_name">
-        <img src={Splita_logo} alt="Splita Logo" onClick={() => navigate('/')} style={{cursor: 'pointer'}}/>
+        <img
+          src={Splita_logo}
+          alt="Splita Logo"
+          onClick={() => navigate("/")}
+          style={{ cursor: "pointer" }}
+        />
       </div>
 
       <div
@@ -95,8 +100,6 @@ const Join_Group = () => {
               onChange={(e) => setInviteLink(e.target.value)}
             />
           </div>
-
-          {error && <p className="error">{error}</p>}
 
           <button type="submit">Join Group</button>
         </form>
